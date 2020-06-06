3b29291f25364f4a03ef5e5043477ba5377394cf
AdriSC <adrsalva@ucm.es>
2020-06-06 23:24:09 +0200

    cambios

--- Model/newTask.js
+++ Model/newTask.js
@@ -1,11 +1,7 @@
-function Task(description){
-    this.description = description;
-}
-
 $(function () {
     let taskList = []; // Array de Strings con las tareas
 
-    const newTaskBt = document.getElementById('newTaskBt');
+    //const newTaskBt = document.getElementById('newTaskBt');
     const newTaskDiv = document.getElementById('newTaskDiv')
     // console.log(newTaskBt);
     if(localStorage.getItem("taskList") !== null){
@@ -14,50 +10,44 @@ $(function () {
         // onsole.log(tasks)
 
         for(let i = 0; i < taskList.length; i++){
-            $('#taskListDiv').append('<button class="task">' + taskList[i] + ' </button>');
+            $('#taskListUl').append('<button class="task">' + taskList[i] + ' </button>');
         }
     }
     else{
         console.log('Not here');
     }
 
-    newTaskBt.addEventListener('click', function (){
-        newTaskBt.disabled = true;
-        const taskText = document.createElement('input');
-        taskText.setAttribute('id', 'taskText');
-        newTaskDiv.appendChild(taskText);
-
-        const saveBt = document.createElement('input');
-        saveBt.setAttribute('type', 'submit');
-        saveBt.setAttribute('id', 'saveBt');
-        saveBt.setAttribute('class', 'buttonStyle');
-        saveBt.innerHTML = 'Guardar';
-        newTaskDiv.appendChild(saveBt);
-
-        const cancelBt = document.createElement('button');
-        cancelBt.setAttribute('id', 'cancelBt');
-        cancelBt.setAttribute('class', 'buttonStyle');
-        cancelBt.innerHTML = 'Cancelar';
-        newTaskDiv.appendChild(cancelBt);
-    });    
+    $('#newTaskBt').on('click', ()=>{
+        $('#newTaskBt').prop('disabled', true);
+        let taskText = $('<input type="text" id="taskText"></input>');
+        let saveBt = $('<button id="saveBt"></button>').text('Guardar');
+        let cancelBt = $('<button id="cancelBt"></button>').text('Cancelar');
+        $('#newTaskDiv').append(taskText, saveBt, cancelBt);
+    }); 
 
     $('body').on('click', '#saveBt', function () { //Click eb el boton de guardar la nueva tarea
         let texto = $('#taskText').val();
-        $('#taskListDiv').append('<button class="task">' + texto + ' </button>');
-        taskList.push(texto);
-        localStorage.setItem("taskList", JSON.stringify(taskList));
-        console.log(taskList);
+        if (texto !== ""){
+            let taskDiv =$('<div id="taskDiv"></div>');
+            $('#taskListUl').append(taskDiv);
+            let task =$('<button class="task">' + texto + ' </button>');
+            taskDiv.append(task);
+            taskList.push(texto);
+            localStorage.setItem("taskList", JSON.stringify(taskList));
+            console.log(taskList);
+        }
     });
 
     $('body').on('click', '#cancelBt', function () { //Click eb el boton de guardar la nueva tarea
-        newTaskBt.disabled = false;
+        $('#newTaskBt').prop('disabled', false);
         $('#saveBt').remove();
         $('#taskText').remove();
         $('#cancelBt').remove();
     });
 
     $('body').on('click', '.task', function () { //Click eb el boton de guardar la nueva tarea
-        let idToDel = $(this).index();
+        let task = $(this).text();
+        let idToDel = taskList.indexOf(task);
         this.remove();
         taskList.splice(idToDel, 1);
         console.log(taskList);
@@ -66,14 +56,3 @@ $(function () {
 
     
 });
-
-/*
-//Gestionarlos después de crearlos. TODO
-document.getElementById('saveBt').addEventListener('click', function(){
-    let task = new Task(document.getElementById('taskText').value);
-    console.log(task)
-});
-
-cancelBt.addEventListener('click', function(){
-
-});*/
