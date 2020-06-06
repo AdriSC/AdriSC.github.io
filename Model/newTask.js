e6d25863fd011d6b85fe457d7664a01dac10a1e4
Mario Torres <48029021+marito07@users.noreply.github.com>
2020-06-06 20:28:38 +0200

    Crear y guardas tasks en el localSotre y mostrarlas

--- Model/newTask.js
+++ Model/newTask.js
@@ -2,10 +2,24 @@ function Task(description){
     this.description = description;
 }
 
-window.onload = function(){
+$(function () {
+    let taskList = []; // Array de Strings con las tareas
+
     const newTaskBt = document.getElementById('newTaskBt');
     const newTaskDiv = document.getElementById('newTaskDiv')
-    console.log(newTaskBt);
+    // console.log(newTaskBt);
+    if(localStorage.getItem("taskList") !== null){
+        //localStorage.setItem("taskList", JSON.stringify(taskList));
+        taskList = JSON.parse(localStorage.getItem("taskList"));
+        // onsole.log(tasks)
+
+        for(let i = 0; i < taskList.length; i++){
+            $('#taskListDiv').append('<button class="task">' + taskList[i] + ' </button>');
+        }
+    }
+    else{
+        console.log('Not here');
+    }
 
     newTaskBt.addEventListener('click', function (){
         newTaskBt.disabled = true;
@@ -13,17 +27,45 @@ window.onload = function(){
         taskText.setAttribute('id', 'taskText');
         newTaskDiv.appendChild(taskText);
 
-        const saveBt = document.createElement('button');
+        const saveBt = document.createElement('input');
+        saveBt.setAttribute('type', 'submit');
         saveBt.setAttribute('id', 'saveBt');
+        saveBt.setAttribute('class', 'buttonStyle');
         saveBt.innerHTML = 'Guardar';
         newTaskDiv.appendChild(saveBt);
 
         const cancelBt = document.createElement('button');
         cancelBt.setAttribute('id', 'cancelBt');
+        cancelBt.setAttribute('class', 'buttonStyle');
         cancelBt.innerHTML = 'Cancelar';
         newTaskDiv.appendChild(cancelBt);
     });    
-}
+
+    $('body').on('click', '#saveBt', function () { //Click eb el boton de guardar la nueva tarea
+        let texto = $('#taskText').val();
+        $('#taskListDiv').append('<button class="task">' + texto + ' </button>');
+        taskList.push(texto);
+        localStorage.setItem("taskList", JSON.stringify(taskList));
+        console.log(taskList);
+    });
+
+    $('body').on('click', '#cancelBt', function () { //Click eb el boton de guardar la nueva tarea
+        newTaskBt.disabled = false;
+        $('#saveBt').remove();
+        $('#taskText').remove();
+        $('#cancelBt').remove();
+    });
+
+    $('body').on('click', '.task', function () { //Click eb el boton de guardar la nueva tarea
+        let idToDel = $(this).index();
+        this.remove();
+        taskList.splice(idToDel, 1);
+        console.log(taskList);
+        localStorage.setItem("taskList", JSON.stringify(taskList));
+    });
+
+    
+});
 
 /*
 //Gestionarlos después de crearlos. TODO
